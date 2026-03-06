7c64864e42b29efd2fbc5b6dffc5ea335a46dcd0
william-fernandes-unity <william.koehler@unity3d.com>
2026-03-06 10:34:23 -0300

    listing countries

--- canvas/lib/main.dart
+++ canvas/lib/main.dart
@@ -1,106 +1,127 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+const RANDOM_COUNRTY_URL = 'https://raw.githubusercontent.com/WillFK/Vexi/refs/heads/main/data/countries.json';
+
+Future<List<Map<String, dynamic>>> fetchCountriesRaw() async {
+  final uri = Uri.parse(RANDOM_COUNRTY_URL);
+  final res = await http.get(
+    uri,
+    headers: {
+      'Accept': 'application/json'
+    }
+  );
+
+  if (res.statusCode < 200 || res.statusCode >= 300) {
+    throw Exception('HTTP ${res.statusCode}: ${res.body}');
+  }
+
+  final decoded = jsonDecode(res.body) as Map<String, dynamic>;
+  final list = decoded['countries'] as List<dynamic>;
+  return list.cast<Map<String, dynamic>>();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+final countriesProvider = FutureProvider<List<Map<String, dynamic>>>((ref) async {
+  return fetchCountriesRaw();
+});
 
-  // This widget is the root of your application.
+void main() {
+  runApp(const ProviderScope(child: App()));
+}
+
+class App extends StatelessWidget {
+
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: "Canvexi",
+      theme: ThemeData(useMaterial3: true),
+      home: CountriesPage()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CountriesPage extends ConsumerWidget {
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const CountriesPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final countries = ref.watch(countriesProvider);
+
+    return Scaffold(
+      appBar: _bar(),
+      body: countries.when(
+          data: (items) {
+            return ListView.separated(
+                itemCount: items.length,
+                separatorBuilder: (_, __) => const Divider(height: 1),
+                itemBuilder: (context, i) {
+                  final c = items[i];
+
+                  // Adapt these keys to your JSON
+                  final code = (c['code'] ?? '').toString();
+                  final fullNameEn = (c['full_name_en'] ?? '').toString();
+                  final shortNameEn = (c['short_name_en'] ?? '').toString();
+
+                  return ListTile(
+                    title: Text(fullNameEn.isEmpty ? shortNameEn : fullNameEn),
+                    subtitle: Text(code),
+                  );
+                }
+            );
+          },
+          error: (e, st) {
+
+            debugPrintStack(stackTrace: st);
+            return ErrorMessage(onRetry: () => ref.invalidate(countriesProvider));
+          },
+          loading: () => _bodyLoading()
+      ),
+    );
+  }
+
+  AppBar _bar() {
+    return AppBar(title: const Text("Canvexi"));
+  }
+
+  Widget _bodyLoading() {
+    return const Center(
+      child: CircularProgressIndicator()
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class ErrorMessage extends StatelessWidget {
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final VoidCallback onRetry;
+
+  const ErrorMessage({
+    super.key,
+    required this.onRetry
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return Center(
+      child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('failed to load!'),
+              SizedBox(height: 12),
+              ElevatedButton(
+                  onPressed: onRetry,
+                  child: const Text('Retry')
+              )
+            ],
+          )
       ),
     );
   }
